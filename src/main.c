7ee2acebd60cbdc8c21d8d5cf69e03ba1abcfd93
Danila Zakharchuk <zakharchukdanila@gmail.com>
2025-11-24 20:27:52 +0100

    hotfix: fix argc check

--- src/main.c
+++ src/main.c
@@ -88,7 +88,7 @@ int main(int argc, char *argv[]) {
     return 0;
   }
 
-  if (strcmp(argv[1], "uninstall") == 0) {
+  if (argc > 1 && strcmp(argv[1], "uninstall") == 0) {
     run_uninstaller();
     return 0;
   }
